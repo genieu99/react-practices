a7f62725de64a49e78466babf0cca02497ac2ae1
genieu99 <yujin0419@sookmyung.ac.kr>
2024-07-18 16:22:20 +0900

    class: route #1

--- 04.Routing/src/01/App.js
+++ 04.Routing/src/01/App.js
@@ -7,6 +7,19 @@ import Error404 from "./component/Error404";
 export default function App() {
     const [route, setRoute] = useState('');
 
+    const handlerHashChange = () => {
+        console.log(window.location.hash);
+        setRoute(window.location.hash.substring(1));
+    }
+
+    useEffect(() => {
+        window.addEventListener("hashchange", handlerHashChange);
+
+        return () => {
+            window.removeEventListener("hashchange", handlerHashChange);
+        }
+    }, []);
+
     return (() => {
         switch(route) {
             case '' :
